743087c3366e301fb5a96a3f3de9001307ba920a
dnstarcert <gendalf1984@gmail.com>
2023-04-29 00:12:43 +0300

    add ssl header

--- cranio/settings.py
+++ cranio/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='1')
-
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
